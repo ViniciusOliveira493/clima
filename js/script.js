df49b61acd636d4f20e914c40ab59536749e5fa6
Vinicius <vinilimadeoliveira@hotmail.com>
2023-09-14 21:34:04 -0300

    Adicionando pesquisa de cidades e favicon

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 window.onload = function () {
     iniciar();
+    traduzir();
 }
 
 document.getElementById("btnSearchLocalWeather").addEventListener("click",function () {
     carregarLocal();
 });
 
+document.getElementById("btnSearch").addEventListener("click",function () {
+    let cidade = document.getElementById("txtCidade").value;
+
+    buscarClimaAtual(0,0,cidade);
+});
+
 function iniciar(){
-    buscarClimaAtual(-23,-46);
-    buscarClimaDias(-23,-46);
+    buscarClimaAtual(0,0,"sao paulo");
 }
 
 function carregarLocal() {
@@ -17,7 +23,6 @@ function carregarLocal() {
         if(authorized){
             navigator.geolocation.getCurrentPosition(function (pos) {
                 buscarClimaAtual(pos.coords.latitude,pos.coords.longitude);
-                buscarClimaDias(pos.coords.latitude,pos.coords.longitude);
             });
         }        
     }else{
@@ -25,24 +30,47 @@ function carregarLocal() {
     }
 }
 
-function buscarClimaAtual(latitude, longitude){
+function buscarClimaAtual(latitude, longitude, city){
     let div = document.getElementById('info');
-    let url = "https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid=0f99fb452e1f092b08e2a0c5e4e0b115";  
-   
+    let url = "";
+    
+    if(city==undefined){
+        url = "https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid=0f99fb452e1f092b08e2a0c5e4e0b115";
+    }else{
+        url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=0f99fb452e1f092b08e2a0c5e4e0b115";
+    }
+
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
-            loadFields1(data);
+            if(data.cod != 404){
+                loadFields1(data);
+                buscarClimaDias(latitude, longitude, city);
+            }else{
+                alert(data.message);
+            }
         });
+    
+    
 }
 
-function buscarClimaDias(latitude, longitude){
-    let url = "https://api.openweathermap.org/data/2.5/forecast?lat="+latitude+"&lon="+longitude+"&appid=0f99fb452e1f092b08e2a0c5e4e0b115";
+function buscarClimaDias(latitude, longitude,city){
+    let url = "";
+
+    if(city==undefined){
+        url = "https://api.openweathermap.org/data/2.5/forecast?lat="+latitude+"&lon="+longitude+"&appid=0f99fb452e1f092b08e2a0c5e4e0b115";
+    }else{
+        url = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=0f99fb452e1f092b08e2a0c5e4e0b115";
+    }
 
     fetch(url)
     .then((response) => response.json())
     .then((data) => {
-        console.log(JSON.stringify(data))
+        let conteiner = document.getElementById("cardConteiner");
+        conteiner.innerHTML = "";
+        data.list.forEach(element => {            
+            conteiner.appendChild(createCard(element));
+        });
     });    
 }
 
@@ -54,14 +82,15 @@ function loadFields1(data){
     let minTemp = document.getElementById('lblMinTemp');
     let fellTemp = document.getElementById('lblFellTemp');
 
-    let humidity = document.getElementById('lblHumidity');
-    let atm = document.getElementById('lblAtmPress');
-    let visibility = document.getElementById('lblVisibility');
-    let wind = document.getElementById('lblWind');
-    let cloud = document.getElementById('lblCloud');
+    let humidity = document.getElementById('lblHumidityNumber');
+    let atm = document.getElementById('lblAtmPressNumber');
+    let visibility = document.getElementById('lblVisibilityNumber');
+    let wind = document.getElementById('lblWindNumber');
+    let cloud = document.getElementById('lblCloudNumber');
 
     let weather = document.getElementById('wheaterIcon');
 
+    document.title = "Clima Atual - "+data.name;
     cidade.innerHTML = data.name;
     cond.innerHTML = data.weather[0].description;
     temp.innerHTML = parseInt((data.main.temp)-273.15);
@@ -77,4 +106,48 @@ function loadFields1(data){
 
     weather.setAttribute("src", "https://openweathermap.org/img/wn/"+data.weather[0].icon+".png");
 
+}
+
+function createCard(data) {
+    let div = document.createElement("div");
+    div.setAttribute("class","card");
+
+    let img = document.createElement("img");
+    img.setAttribute("src","https://openweathermap.org/img/wn/"+data.weather[0].icon+".png");
+
+    img.setAttribute("alt","weather icon");
+
+    div.appendChild(img);
+
+    let graus = document.createElement("span");
+    graus.innerHTML = parseInt((data.main.temp)-273.15)+"ºC <br>";
+
+    div.appendChild(graus);
+    let date = document.createElement("span");
+    
+    let dataEHora = data.dt_txt;
+    dataEHora = dataEHora.split(" ");
+
+    let dataCard = dataEHora[0].split("-");
+    dataCard =  new Date(dataCard[0],dataCard[1]-1,dataCard[2]);   
+
+    date.innerHTML = formatarData(dataCard)+" "
+                    +dataEHora[1].substring(0,dataEHora[1].length-3);
+
+    div.appendChild(date);
+    return div;
+}
+
+function formatarData(dataCard){
+    let diaCard = dataCard.getDate();
+    if(diaCard < 10){
+        diaCard = "0"+diaCard;
+    }
+
+    let mesCard = dataCard.getMonth();
+    if (mesCard < 10) {
+        mesCard = "0"+mesCard;
+    }
+
+    return diaCard+"/"+mesCard+"/"+dataCard.getFullYear();
 }
--- js/traducao/traducao.js
+++ js/traducao/traducao.js
@@ -1,1 +1,45 @@
-console.log("tradução iniciada......")
+function traduzir() {
+    const language = window.navigator.language;
+    buscarArquivo(language);
+}
+
+function buscarArquivo(lang) {
+    fetch("./js/traducao/lang/"+lang+".json")
+        .then(response => response.json())
+        .then(json => {
+            mudarTextos(json);            
+        })
+        .catch(err => {
+            fetch("./js/traducao/lang/en-US.json")
+                .then(response => response.json())
+                .then(json => {
+                    mudarTextos(json);            
+                });
+        });    
+}
+
+function mudarTextos(frases){
+    let txtCidade = document.getElementById("txtCidade");
+    let lblTemp = document.getElementById("lblTemp");
+    let lblMax = document.getElementById("lblMax");
+    let lblMin = document.getElementById("lblMin");
+    let lblFeel = document.getElementById("lblFeel");
+    let lblHumidity = document.getElementById("lblHumidity");
+    let lblAtmPress = document.getElementById("lblAtmPress");
+    let lblVisibility = document.getElementById("lblVisibility");
+    let lblWind = document.getElementById("lblWind");
+    let lblWindSpeed = document.getElementById("lblWindSpeed");
+    let lblCloud = document.getElementById("lblCloud");
+
+    txtCidade.setAttribute("placeholder",frases.site.txtSearch);
+    lblTemp.innerHTML = frases.site.lblTemp;
+    lblMax.innerHTML = frases.site.lblMax;
+    lblMin.innerHTML = frases.site.lblMin;
+    lblFeel.innerHTML = frases.site.lblFeel;
+    lblHumidity.innerHTML = frases.site.lblHumidity;
+    lblAtmPress.innerHTML = frases.site.lblAtmPress;
+    lblVisibility.innerHTML = frases.site.lblVisibility;
+    lblWind.innerHTML = frases.site.lblWind;
+    lblWindSpeed.innerHTML = frases.site.lblWindSpeed;
+    lblCloud.innerHTML = frases.site.lblCloud;
+}
